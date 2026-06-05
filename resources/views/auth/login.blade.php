21df7f7665a6edefb8692325dd31eb431bb7564c
neilclarencesajorda-cloud <neilclarence.sajorda@cvsu.edu.ph>
2026-06-05 11:03:57 +0800

    Change the color of sidebar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,7 @@
     <title>Medicine System - Login</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 </head>
 <body class="bg-light d-flex justify-content-center align-items-center" style="min-height:100vh;">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,6 +6,7 @@
     <title>Medicine System - Register</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
 
 </head>
