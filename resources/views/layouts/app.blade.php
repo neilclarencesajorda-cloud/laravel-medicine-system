554a891bc20b319e6a9d8960dce775eb963185f0
neilclarencesajorda-cloud <neilclarence.sajorda@cvsu.edu.ph>
2026-06-05 11:25:11 +0800

    make responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,7 +95,7 @@
 
         </div>
 
-        <div class="main-content p-4 flex-grow-1">
+        <div class="main-content flex-grow-1" style="min-width:0; overflow-x:auto;">
             @yield('content')
         </div>
 
